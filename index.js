da1d15dfe597224151cc5950806845e3ad80df1e
luchodeco <72599025+louisdeco@users.noreply.github.com>
2025-11-25 14:26:00 +0400

    feat: add Node.js HTTP server with basic routing

---
+++ index.js
@@ -0,0 +1,41 @@
+import http from "node:http";
+import { URL } from "node:url";
+import { readFile } from "node:fs/promises";
+
+// Serve the file
+async function serveFile(filename, statusCode, response) {
+    try {
+        const data = await readFile(filename, 'utf8');
+        response.writeHead(statusCode, { 'Content-Type': 'text/html' });
+        response.end(data);
+    } catch (err) {
+        console.log(err);
+        response.writeHead(500, { 'Content-Type': 'text/plain' });
+        response.end('Server error');
+    }
+}
+
+// Create a server with a request handler
+const server = http.createServer(async (request, response) => {
+  // Parse the request URL
+  const myUrl = new URL(request.url, `http://${request.headers.host}`);
+  
+  console.log('Path:', myUrl.pathname);
+  console.log('Query:', myUrl.searchParams.toString());
+  console.log('URL', myUrl);
+
+  if (myUrl.pathname === '/') {
+    await serveFile('index.html', 200, response);
+  } else if (myUrl.pathname === '/about') {
+    await serveFile('about.html', 200, response);
+  } else if (myUrl.pathname === '/contact-me') {
+    await serveFile('contact-me.html', 200, response);
+  } else {
+    await serveFile('404.html', 404, response);
+  }
+});
+
+// Start the server listening on port 3000
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000/');
+});
